4b53e49a4ae5d7a9adaa3b4d01be2d4ec27a4ffe
aubrcart224 <aubrcart224@ed.amdsb.ca>
2024-12-27 23:32:10 -0500

    getting there

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,29 +2,31 @@
 
 import Link from "next/link"
 import { cn } from "@/lib/utils"
+import { FadeText } from "../components/fade-text"
+
 
 const blogPosts = [
   { 
     id: 1, 
-    title: "motivating adam optimizer",
+    title: "Soon to be blog post",
     date: "sept 16, 2024",
     slug: "motivating-adam-optimizer"
   },
   { 
     id: 2, 
-    title: "parser theory",
+    title: "Soon to be blog post",
     date: "sept 2, 2024",
     slug: "parser-theory"
   },
   { 
     id: 3, 
-    title: "Writing a Lexer",
+    title: "Soon to be blog post",
     date: "aug 25, 2024",
     slug: "writing-a-lexer"
   },
   { 
     id: 4, 
-    title: "motivating ppo algo",
+    title: "Soon to be blog post",
     date: "june 24, 2024",
     slug: "motivating-ppo-algo"
   },
@@ -34,12 +36,18 @@ export default function BlogPage() {
   return (
     <>
       <div className="w-full max-w-2xl mx-auto">
-        <h1 className="text-2xl font-medium text-center mb-8">Aubrey Carter</h1>
+        <h1 className="text-4xl font-bold text-center mb-16">
+          <FadeText>
+            Blog posts
+          </FadeText>
+        </h1>
         <div className="space-y-4">
           {blogPosts.map((post) => (
             <div key={post.id} className="flex justify-between items-center group">
+             
               <Link 
-                href={`/blog/${post.slug}`}
+                //href={`/blog/${post.slug}`}
+                href={`/blog/`}
                 className={cn(
                   "text-purple-400 hover:text-purple-400 transition-colors",
                   "font-mono text-sm underline underline-offset-4"
@@ -47,6 +55,7 @@ export default function BlogPage() {
               >
                 {post.title}
               </Link>
+              
               <span className="text-white font-mono text-sm">
                 {post.date}
               </span>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,12 +1,17 @@
 'use client'
 
 import { ProjectCarousel } from "@/app/components/project-carousel"
+import { FadeText } from "@/app/components/fade-text"
 
 export default function ProjectsPage() {
   return (
     <>
       <div className="w-full max-w-4xl mx-auto">
-        <h1 className="text-4xl font-bold text-center mb-4">Projects</h1>
+        <h1 className="text-4xl font-bold text-center mb-4">
+          <FadeText>
+            Projects
+          </FadeText>
+          </h1>
         <ProjectCarousel />
       </div>
     </>
